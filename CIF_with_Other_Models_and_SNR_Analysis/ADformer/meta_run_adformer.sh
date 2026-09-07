8e9c761cd1ab7705234ccb119110031a770feee8
Ming Hu <111221434+Xi-Mu-Yu@users.noreply.github.com>
2026-09-07 18:06:45 +0800

    Delete CIF_with_Other_Models_and_SNR_Analysis directory

--- CIF_with_Other_Models_and_SNR_Analysis/ADformer/meta_run_adformer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run scripts sequentially
-
-# ADformer
-bash ./scripts/ADformer/supervised/ADformer/AD-VS-HC/S-1.sh
-bash ./scripts/ADformer/supervised/ADformer/AD-VS-NonAD/S-1.sh
-bash ./scripts/ADformer/supervised/ADformer/HC-VS-Abnormal/S-1.sh
-bash ./scripts/ADformer/supervised/ADformer/Multi-Class/S-1.sh
